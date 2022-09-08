167d84d1d6f5a3c9b2cae7db0b85d4c8ef0a900a
mpietryka <mat.pietryka@gmail.com>
2022-09-08 16:26:02 +0100

    .

--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
-import { stringify } from "uuid";
 import {
   Heading,
   Btn,
